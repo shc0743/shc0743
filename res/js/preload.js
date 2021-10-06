26aa9eed46ea0f4181f243df4f0a338cf0a534f6
shc0743 <shc0743@outlook.com>
2021-10-06 17:42:53 +0800

    Update

---
+++ res/js/preload.js
@@ -0,0 +1,17 @@
+(function (window) {
+    // function resize__handle_width_height_full_el() {
+    //     let _width_full_el = window.document.querySelectorAll('.width-full');
+    //     let _len = _width_full_el.length;
+    //     let _doc_width = window.document.documentElement.clientWidth;
+    //     for (let i = 0; i < _len; ++i)
+    //         _width_full_el[i].style.width = _doc_width + 'px';
+        
+    //     let _height_full_el = window.document.querySelectorAll('.height-full');
+    //     _len = _height_full_el.length;
+    //     let _doc_height = window.document.documentElement.clientHeight;
+    //     for (let i = 0; i < _len; ++i)
+    //         _heith_full_el[i].style.height = _doc_height + 'px';
+    // };
+    // window.addEventListener('resize', resize__handle_width_height_full_el);
+    // resize__handle_width_height_full_el();
+})(window.self);
