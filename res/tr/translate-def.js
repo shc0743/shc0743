eb78203242801608e6f3c1dc3b093f090a4c71e0
shc0743 <shc0743@outlook.com>
2021-10-07 15:19:19 +0800

    Debug

--- res/tr/translate-def.js
+++ res/tr/translate-def.js
@@ -1,6 +1,6 @@
 window.translate_location = "en-US";
 window.translate_location_fetch3 =
-    fetch("res/tr/translate_location.json").then(function (v) { return v.json(); })
+    fetch("res/tr/translate-location.json").then(function (v) { return v.json(); })
     .then(function (v) {
         window.translate_location = v[site_data.language] ? v[site_data.language] : v['en-US'];
         return translate_location;
